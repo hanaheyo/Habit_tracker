fffbc9dd8344d12c63070627b1c7f64824f06a70
hanaheyo <violets5618@gmail.com>
2020-10-19 18:36:18 +0900

    Habit component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
+import '@fortawesome/fontawesome-free/js/all.js';
 
 // root에 app이라는 component를 연결하는 것.
 ReactDOM.render(
