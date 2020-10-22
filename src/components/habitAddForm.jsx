8254a742a21e7ae53ca481bc50f8955265b77e1e
hanaheyo <violets5618@gmail.com>
2020-10-22 16:44:13 +0900

    Add Form

---
+++ src/components/habitAddForm.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class HabitAddForm extends Component {
+    formRef = React.createRef();
+    inputRef = React.createRef();
+
+    onSubmit = event => {
+        event.preventDefault(); // submit 됐을 때, 기본으로 refresh되는 것 방지
+        const name = this.inputRef.current.value;
+        name && this.props.onAdd(name);
+        // this.inputRef.current.value = '';
+        this.formRef.current.reset();
+    };
+
+    render() {
+        return (
+            <form ref={this.formRef} className="add-form" onSubmit={this.onSubmit}>
+                <input 
+                    ref={this.inputRef}
+                    type="text" 
+                    className="add-input" 
+                    placeholder="please enter yout habit"
+                />
+                <button className="add-button">Add</button>
+            </form>
+        );
+    }
+}
+
+export default HabitAddForm;
